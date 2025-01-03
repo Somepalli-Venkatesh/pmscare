02a5277ea6e3c2d0b9e2b5ebbbacd93bb2f6b1db
Somepalli-Venkatesh <venkateshsomepalli999@gmail.com>
2025-01-03 12:01:42 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,11 +1,11 @@
 export const GenderOptions = ["Male", "Female", "Other"];
 
 export const PatientFormDefaultValues = {
-  // firstName: "",
-  // lastName: "",
-  // email: "",
-  // phone: "",
-  // birthDate: new Date(Date.now()),
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  birthDate: new Date(Date.now()),
   gender: "Male" as Gender,
   address: "",
   occupation: "",
